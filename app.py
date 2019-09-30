bff4917c94755316626c63bb64906e080eb466f7
= <zainr7989@gmail.com>
2019-09-29 17:04:25 -0700

    Update root route to display Resources.

--- app.py
+++ app.py
@@ -1,13 +1,51 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-FLASK_APP = app  # specifying flask app
+# FLASK_APP = app  # specifying flask app
+
+# MOCK ARRAY of INVESTMENT DEALS
+
+# list of links for property images
+links = [
+    "https://photos.zillowstatic.com/cc_ft_960/ISnudtiomyixgn0000000000.jpg",
+    "https://photos.zillowstatic.com/cc_ft_960/ISfciedjxtcf841000000000.jpg",
+    "https://photos.zillowstatic.com/cc_ft_960/ISatojh6728i6o1000000000.jpg",
+    "https://photos.zillowstatic.com/p_e/IS6eqr7il2cjge0000000000.jpg"
+]
+investments = [
+    {'name': 'Dallas, TX Property',
+     'picture': links[0],
+     'address': '4432 Bowser Ave, Dallas, TX 75219',
+     'value': 1500000,
+     'status': 'OPEN',
+     'num_units': 11},
+    {'name': 'Austin, TX Property',
+     'picture': links[1],
+     'address': '701 Baylor St, Austin, TX 78703',
+     'value': 1700000,
+     'status': 'OPEN',
+     'num_units': 6},
+    {'name': 'Fort Worth, TX Property',
+     'picture': links[2],
+     'address': '10294 Western Oaks Rd, Fort Worth, TX 76108',
+     'value': 2700000,
+     'status': 'CLOSED',
+     'num_units': 12},
+    {'name': 'Houston, TX Property',
+     'picture': links[3],
+     'address': '3709 Montrose Blvd, Houston, TX 770069',
+     'value': 1000000,
+     'status': 'CLOSED',
+     'num_units': 7},
+]
 
 
 @app.route('/')
-def index():
-    """Return homepage."""
-    return render_template('home.html', msg="Welcome to Homely Properties")
+def investments_index():
+    """Show all open and closed investment properties."""
+    return render_template('investments_index.html',
+                           investments=investments,
+                           links=links)
 
 
 if __name__ == '__main__':
