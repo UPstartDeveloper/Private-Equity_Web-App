ab8bb73486d19d59e56dbe48c17a652639bacc4c
= <zainr7989@gmail.com>
2019-10-06 21:20:38 -0700

    [ADD] Users see single offers.

--- app.py
+++ app.py
@@ -61,8 +61,8 @@ def offers_new():
     '''Render form to enter offer on a property.'''
     return render_template('offers_new.html', properties=properties)
 
-
-@app.route('/offers_show', methods=['GET', 'POST'])
+'''
+@app.route('/offers_new', methods=['GET', 'POST'])
 def offers_show():
     """Submit a new offer on a location to make an investment.
        User sees all offers made so far on investment properties.
@@ -75,16 +75,36 @@ def offers_show():
         "location": request.form.get('location')
     }
     # Insert into PyMongo database
-    offers.insert_one(new_offer)
+    offer_id = offers.insert_one(new_offer).inserted_id
 
-    # display all previous offers
-    return render_template('offers_show.html', offers=offers.find())
+    # redirect to page showing new offer only
+    return render_template('offers_show.html', offer_id=offer_id)
+'''
 
-
-@app.route('/offers_show', methods=['GET'])
+@app.route('/offers_show', methods=['GET', 'POST'])
 def offers_show_all():
-    """Users sees all previously made offers on a property from other users."""
+    """Submit a new offer on a location to make an investment.
+       Users sees all previously made offers on a property from other users.
+    """
+    # Make a new JSON from form data
+    new_offer = {
+        "name": request.form.get('name'),
+        "offer": request.form.get('offer'),
+        "email": request.form.get('email'),
+        "location": request.form.get('location')
+    }
+    # Insert into PyMongo database
+    offer_id = offers.insert_one(new_offer).inserted_id
     return render_template('offers_show.html', offers=offers.find())
+
+# New routes below 10/6/19
+
+
+@app.route('/offers/<offer_id>')
+def offers_show_single(offer_id):
+    """Show a single offer."""
+    offer = offers.find_one({'_id': ObjectId(offer_id)})
+    return render_template('offer_show.html', offer=offer)
 
 
 @app.route('/offers/<offer_id>/edit')
@@ -94,7 +114,7 @@ def offers_edit(offer_id):
     return render_template('offers_edit.html', offer=offer,
                            properties=properties)
 
-
+'''
 @app.route('/offers_show', methods=['POST'])
 def offers_update(offer_id):
     """Submit an edited offer."""
@@ -110,6 +130,6 @@ def offers_update(offer_id):
     return redirect(url_for('offers_show', offer_id=offer_id,
                     offers=offers.find()))
 
-
+'''
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
